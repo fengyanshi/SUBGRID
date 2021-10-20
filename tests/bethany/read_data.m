7b179847776c4cc9dd6113c88e46242cce75ae7e
fengyanshi <fyshi.gbox@gmail.com>
2021-10-20 10:49:13 -0400

    change ft to meter

--- tests/bethany/read_data.m
+++ tests/bethany/read_data.m
@@ -9,7 +9,7 @@ ele=T.Var5;
 time_num=(time_num-time_num(1))*24.0*3600;
 
 writeout(:,1)=time_num;
-writeout(:,2)=ele;
+writeout(:,2)=ele*0.3048;
 
 fid = fopen('gauge_data.txt', 'wt');
   fprintf(fid, ['%5f %5f', '\n'], writeout');
